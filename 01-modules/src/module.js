968f7cb07fb0cc2585ed4fe74d4c63468eeb339e
Daniel Witek <dwitek.mmt-b2015@fh-salzburg.ac.at>
2016-11-06 23:39:47 +0100

    finished 01-modules

--- 01-modules/src/module.js
+++ 01-modules/src/module.js
@@ -1,15 +1,37 @@
 export function valid(email) {
-
+	var succeed = '^[a-zA-Z]+\.[a-z]{3}-[bm][0-9]{4}@fh-salzburg.ac.at';
+	var ergebnis = email.match(succeed);
+	if (ergebnis){
+		return true;
+	}
+	return false;
 }
 
 export function degreeProgram(email) {
-
+	if (valid(email)){
+		var chopped = email.split('.');
+		var degree = chopped[1].split('-');
+		return degree[0].toUpperCase();
+	}
 }
 
 export function level(email) {
-
+	if (valid(email)){
+		var level = '[bm][0-9]{4}';
+		var ergebnis = email.match(level);
+		if (ergebnis[0]=='m') {
+			return "MA";
+		}
+		return "BA";
+	}
 }
 
 export function graduationYear(email) {
-
+	var year = '[0-9]{4}';
+	var gradyear = email.match(year);
+	if (level(email)=="BA"){
+		return parseInt(gradyear[0]) + 3;
+	}
+	return parseInt(gradyear[0]) + 3;
+	
 }
