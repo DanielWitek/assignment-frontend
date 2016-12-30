62cc8eeba158faffae5ea537c61241af6a37b4e8
Daniel Witek <dwitek.mmt-b2015@fh-salzburg.ac.at>
2016-12-30 16:44:59 +0100

    router

--- 02-router/src/router.js
+++ 02-router/src/router.js
@@ -1,69 +1,45 @@
-import $ from 'jquery'
 var pathObject = {};
 
-export default function(page, fun) {
-    if(page && fun)
-    {
+export default function (page, fun) {
+    if (page && fun) {
         pathObject[page] = fun;
+        const route = page.split('/').map(function (part) {
+            if (part.substr(0, 1) !== ':') {
+                return part
+            }
+            const name = part.substr(1)
+            return '(a-z)+'
+        }).join('\/')
+        pathObject[route] = fun;
         return;
     }
-    else
-    {
+    else {
         start();
         addClickListener();
     }
 }
 
-function addClickListener()
-{
-    $( 'a' ).on( "click", function(event) {
+function addClickListener() {
+    $('a').on("click", function (event) {
         event.preventDefault();
-        var data = event.target.getAttribute('href');
-        history.pushState(null, null, data);
-        directTo(data);
     });
 }
 
-function start()
-{
-    //return player("magnus");
+function start() {
     let path = window.location.pathname;
-    //console.log(path);
     directTo(path);
 }
 
-function directTo(newpath)
-{
+function directTo(newpath) {
+    if (pathObject[newpath]) {
+        return pathObject[newpath]();
+    }
     const matchPlayer = /(\/players\/)([a-zA-Z]+)/g;
-    let path = newpath;
 
-    if(newpath.match(matchPlayer))
-    {
-        var playerId = matchPlayer.exec(newpath)[2];
-        //console.log(newpath);
-         //console.log(pathObject);
-         //console.log(playerId);
-          //return url(playerId);
-        if (newpath in pathObject )
-        {
-            console.log(true);
-           
+    for (var i in pathObject) {
+        if (pathObject[i] === newpath) {
+            return pathObject[i]();
         }
-
-        /*if (window.history.state)
-        {
-            let lastviewed = window.history.state.pathname;
-
-            if(path != lastviewed)
-            {
-                window.history.pushState({
-                    pathname: path
-                }, '', path)
-            }
-
-            let val = pathObject.get(route);
-            return val(url);
-        }*/
     }
 
 }
